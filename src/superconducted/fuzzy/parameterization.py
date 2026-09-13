0da080da6a98bd297c40c1d7f57500b9cc82d1a4
yigit-arda <yigit9188@gmail.com>
2026-09-14 01:56:32 +0300

    Apply automatic ruff formatting to parameterization.py

--- src/superconducted/fuzzy/parameterization.py
+++ src/superconducted/fuzzy/parameterization.py
@@ -205,19 +205,16 @@ def grid_partition(
 
     elif shape is TriangularMF:
         for j in range(k):
-
             mfs.append(TriangularMF(e[j], c[j], e[j + 1]))
 
     elif shape is TrapezoidalMF:
         for j in range(k):
-
             m_l = (c[j] - e[j]) / 4.0
             m_r = (e[j + 1] - c[j]) / 4.0
             mfs.append(TrapezoidalMF(e[j], c[j] - m_l, c[j] + m_r, e[j + 1]))
 
     elif shape is TanhBellMF:
         for j in range(k):
-
             m_l = (c[j] - e[j]) / 4.0
             m_r = (e[j + 1] - c[j]) / 4.0
             min_m = min(m_l, m_r)
